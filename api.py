2dfd34e1f4f8658c2f9d6229d2c0ff0032cca0c7
Ari Häyrinen <ari.hayrinen@gmail.com>
2024-10-16 20:58:04 +0300

    fixed chunking so that start and end values match to original text

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, send_file
 from argparse import ArgumentParser
 from transformers import pipeline
 import syntok.segmenter as segmenter
+import re
 
 # This adds the MessyDeskAPI endpoint to Finbert-ner via transformers 
 
@@ -46,17 +47,29 @@ def process_files():
     try:
         output_id = str(uuid.uuid4())
         text = text_file.read().decode('utf-8')
-        sentences = split_into_sentences(text)
+        sentences = split_text_into_chunks(text, 4000)
         predictions = token_classifier(sentences)
         print(predictions)
-        # concat predictions to one array
-        predictions = [item for sublist in predictions for item in sublist]
+
+        # concat predictions to one array and adjust "start" and "end" values so that they match the original text
+        output = []
+        offset = 0
+        for idx, p in enumerate(predictions):
+
+            for w in p:
+                w['start'] = w['start'] + offset - 1
+                w['end'] = w['end'] + offset - 1
+                output.append(w)
+
+            offset += len(sentences[idx]) 
+
         with open(os.path.join(OUTPUT_FOLDER, output_id + '.ner.json'), "w") as file:
-            json.dump(str(predictions), file, indent=4)
+            json.dump(str(output), file, indent=4)
 
 
 
     except Exception as e:
+        print(e)
         return jsonify({'error': 'Predictions failed: {}'.format(str(e))}), 500
 
     return jsonify({'response':{'type': 'stored', 'uri': ['/files/' + output_id + '.ner.json']}}), 200
@@ -71,6 +84,30 @@ def serve_file(filename):
     else:
         return jsonify({'error': 'File not found'}), 404
 
+
+def split_text_into_chunks(text, max_length=200):
+    # Split the text into sentences using a regex that matches sentence endings.
+    sentence_endings = re.compile(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s')
+    sentences = sentence_endings.split(text)
+
+    chunks = []
+    current_chunk = ""
+
+    for sentence in sentences:
+        # If adding this sentence to the current chunk would exceed the max_length,
+        # then save the current chunk and start a new one.
+        if len(current_chunk) + len(sentence) + 1 > max_length:  # +1 for space/newline between sentences
+            chunks.append(current_chunk)
+            current_chunk = sentence
+        else:
+            # Add the sentence to the current chunk
+            current_chunk += " " + sentence
+
+    # Add the last chunk if there's any leftover content
+    if current_chunk:
+        chunks.append(current_chunk)
+
+    return chunks
 
 
 def split_into_sentences(text):
